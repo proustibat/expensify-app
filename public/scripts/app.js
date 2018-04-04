bd2e901a9d83d586881bd29ee73fd1e7561bca21
Jennifer Proust <j.proust@.meetic-corp.com>
2018-04-04 15:12:05 +0200

    exploring jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,56 @@
 
 console.log('App.js is running!');
 
-// JSX - Javascript XML
-var template = React.createElement(
-  'h1',
-  null,
-  'This is JSX from app.js'
-);
 var appRoot = document.querySelector('#app');
 
-ReactDOM.render(template, appRoot);
+// JSX - Javascript XML
+var template1 = React.createElement(
+    'div',
+    { key: 'template1' },
+    React.createElement(
+        'h1',
+        null,
+        'This is JSX from app.js'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var template2 = React.createElement(
+    'div',
+    { key: 'template2' },
+    React.createElement(
+        'h1',
+        null,
+        'Jenni'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 32'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Paris'
+    )
+);
+
+ReactDOM.render([template1, template2], appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 console.log( 'App.js is running!' );
 
-// JSX - Javascript XML
-const template = <h1>This is JSX from app.js</h1>;
 const appRoot = document.querySelector( '#app' );
 
-ReactDOM.render( template, appRoot );
+// JSX - Javascript XML
+const template1 = (
+    <div key='template1'>
+        <h1>This is JSX from app.js</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+const template2 = (
+  <div  key='template2'>
+      <h1>Jenni</h1>
+      <p>Age: 32</p>
+      <p>Location: Paris</p>
+  </div>
+);
+
+ReactDOM.render( [
+    template1,
+    template2
+], appRoot );

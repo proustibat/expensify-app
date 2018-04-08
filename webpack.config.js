41e4dccf17aa076d599964c6280c50961d7854ae
Jennifer Proust <j.proust@.meetic-corp.com>
2018-04-08 17:11:43 +0200

    Webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -16,5 +16,9 @@ module.exports = {
         } ]
     },
 
-    devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+
+    devServer: {
+        contentBase: path.join( __dirname, 'public' )
+    }
 };

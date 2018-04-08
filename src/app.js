f7fba5d821e6d1610fb416a251c09a4acddc31dc
Jennifer Proust <j.proust@.meetic-corp.com>
2018-04-08 16:25:38 +0200

    Importing nmp modules

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('jkljkl');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p', {}, 'testing');
+ReactDOM.render( template, document.querySelector( '#app' ) );
+
+

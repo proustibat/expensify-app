9799a28896e065fde5cf5f3d7aef0587c8eea3de
Jennifer Proust <j.proust@.meetic-corp.com>
2018-04-08 20:42:15 +0200

    Setting up webpack with scss

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
+import './styles/styles.scss';
+
 ReactDOM.render( <IndecisionApp />, document.querySelector( '#app' ) );
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,13 @@ module.exports = {
             loader: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
+        }, {
+            test: /\.scss$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
         } ]
     },
 

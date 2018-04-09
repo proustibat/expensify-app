e6405ede14fb72ee7c305ebf5b36175d735502d7
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-09 17:27:50 +0200

    Setting up a 404

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -21,9 +21,13 @@ const HelpPage = () => (
     <div>This is from my Help component</div>
 );
 
+const NotFoundPage = () => (
+    <div>404 Component</div>
+);
+
 const routes = (
     <BrowserRouter>
-        <div>
+        <Switch>
             <Route
                 exact = { true }
                 path = "/"
@@ -41,7 +45,8 @@ const routes = (
                 path = "/help"
                 component = { HelpPage }
             />
-        </div>
+            <Route component = { NotFoundPage } />
+        </Switch>
     </BrowserRouter>
 );
 

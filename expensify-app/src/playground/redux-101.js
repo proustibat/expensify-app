263776c7e1556b2b29629cb21d72171c0907a9b4
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-10 12:47:46 +0200

    Subscribing and dynamic actions

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,30 +1,38 @@
 import { createStore } from "redux";
 
 const store = createStore( ( state = { count: 0 }, action ) => {
-    console.log( 'running: ', action );
-
     switch ( action.type ) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
                 count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
             };
         default:
             return state;
     }
 } );
 
-console.log( store.getState() );
+
+const unsubscribe = store.subscribe( ( ) => {
+    console.log( store.getState() );
+} );
 
 store.dispatch( {
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 } );
 
 store.dispatch( {
@@ -39,5 +47,12 @@ store.dispatch( {
     type: 'DECREMENT'
 } );
 
+store.dispatch( {
+    type: 'DECREMENT',
+    decrementBy: 10
+} );
 
-console.log( store.getState() );
+store.dispatch( {
+    type: 'SET',
+    count: 101
+} );

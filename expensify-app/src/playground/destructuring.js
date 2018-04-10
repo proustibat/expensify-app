5407b73c99df49577357a85537d4a5edff93c548
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-10 14:42:06 +0200

    ES6 Array destructuring

--- expensify-app/src/playground/destructuring.js
+++ expensify-app/src/playground/destructuring.js
@@ -1,3 +1,6 @@
+// Object destructuring
+// ----------------------
+
 // const person = {
 //     name: 'Jenni',
 //     age: 32,
@@ -15,19 +18,38 @@
 //     console.log(`It's ${ temperature } in ${ city }.`);
 // }
 
-const book = {
-    title: 'Ego is the enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
 
-const { name: publisherName = 'Self-Published' } = book.publisher || {};
-
-console.log( `${ publisherName }` ); // Penguin, default: Self-Published
-
+// const book = {
+//     title: 'Ego is the enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
+// const { name: publisherName = 'Self-Published' } = book.publisher || {};
+// console.log( `${ publisherName }` ); // Penguin, default: Self-Published
 
 
+
+// Array destructuring
+// ----------------------
+
+// const address = [
+//     '1299 S Juniper Street',
+//     'Philadelphia',
+//     'Pennslyvania',
+//     '19147'
+// ];
+// const [ , city, state = 'New York' ] = address;
+// console.log( `You are in ${ city } ${ state }` );
+
+const item = [
+  'Coffea (hot)',
+  '$2.00',
+  '$2.50',
+  '$2.75'
+];
+const [ drink, , price  ] = item;
+console.log( `A medium ${ drink } costs ${ price }` );
 
 

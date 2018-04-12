91effaa3de1897f7caa0e4410789fd33be99049c
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-12 16:22:24 +0200

    Redux dev tools

--- expensify-app/src/store/configureStore.js
+++ expensify-app/src/store/configureStore.js
@@ -7,7 +7,9 @@ export default () => {
         combineReducers( {
             expenses: expensesReducer,
             filters: filtersReducer
-        } )
+        } ),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
     );
     return store;
 }

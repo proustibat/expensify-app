068b8324faa6e083bab08687aca208cef19fc247
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-13 10:53:27 +0200

    Setting up Jest

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from "./actions/expenses";
+// import { addExpense } from "./actions/expenses";
 import { setTextFilter } from "./actions/filters";
 import getVisibleExpenses from "./selectors/expenses";
 
@@ -15,9 +15,9 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch( addExpense( { description: 'Water Bill', amount: 4500, createdAt: 1000 } ) );
-store.dispatch( addExpense( { description: 'Gas Bill', amount: 100 , createdAt: 3000 } ) );
-store.dispatch( addExpense( { description: 'Rent', amount: 109500, createdAt: 2000 } ) );
+// store.dispatch( addExpense( { description: 'Water Bill', amount: 4500, createdAt: 1000 } ) );
+// store.dispatch( addExpense( { description: 'Gas Bill', amount: 100 , createdAt: 3000 } ) );
+// store.dispatch( addExpense( { description: 'Rent', amount: 109500, createdAt: 2000 } ) );
 // store.dispatch( setTextFilter( 'bill' ) );
 // store.dispatch( setTextFilter( 'water' ) );
 
@@ -25,10 +25,10 @@ store.dispatch( addExpense( { description: 'Rent', amount: 109500, createdAt: 20
 //     store.dispatch( setTextFilter( 'water' ) );
 // }, 3000 );
 
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses( state.expenses, state.filters );
-console.log( state );
-console.log( visibleExpenses );
+// const state = store.getState();
+// const visibleExpenses = getVisibleExpenses( state.expenses, state.filters );
+// console.log( state );
+// console.log( visibleExpenses );
 
 const jsx = (
     <Provider store = { store }>

a9de0e01ec786c7a5cbbb8f38d716e5ffa8b6013
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-13 17:41:39 +0200

    Enzyme

--- expensify-app/src/tests/components/Header.test.js
+++ expensify-app/src/tests/components/Header.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactShallowRenderer from "react-test-renderer/shallow";
+import { shallow } from 'enzyme';
+// import ReactShallowRenderer from "react-test-renderer/shallow";
 import Header from '../../components/Header';
 
 test( 'should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render( <Header /> );
-    expect( renderer.getRenderOutput() ).toMatchSnapshot();
+    const wrapper = shallow( <Header /> );
+    expect( wrapper ).toMatchSnapshot();
+
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render( <Header /> );
+    // expect( wrapper.find( 'h1' ).text() ).toBe( 'Expensify' );
+
 } );

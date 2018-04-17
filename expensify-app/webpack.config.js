3306f75176d599b362d35cc42e94978853a3e47c
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-17 18:38:32 +0200

    Creating separate css files

--- expensify-app/webpack.config.js
+++ expensify-app/webpack.config.js
@@ -1,8 +1,10 @@
 const path = require( 'path' );
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 
 module.exports = ( env ) => {
 
     const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin( 'styles.css' );
 
     return {
         entry: './src/app.js',
@@ -19,15 +21,30 @@ module.exports = ( env ) => {
                 exclude: /node_modules/
             }, {
                 test: /\.(s*)css$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract( {
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                } )
             } ]
         },
 
-        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        plugins: [
+          CSSExtract
+        ],
+
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
 
         devServer: {
             contentBase: path.join( __dirname, 'public' ),

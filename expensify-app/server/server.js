7141b96a86c9b104d6b139c548b7d07aeeb94730
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-18 15:14:07 +0200

    Deploying to heroku

--- expensify-app/server/server.js
+++ expensify-app/server/server.js
@@ -3,6 +3,7 @@ const express = require( 'express' );
 
 const app = express();
 const publicPath = path.join( __dirname, '..', 'public' );
+const port = process.env.PORT || 3000;
 
 app.use( express.static( publicPath ) );
 
@@ -10,6 +11,6 @@ app.get( '*', (req, res) => {
     res.sendFile( path.join( publicPath, 'index.html') );
 } );
 
-app.listen( 3000, () => {
-    console.log( 'Server is up!' );
+app.listen( port, () => {
+    console.log( `Server is running on port ${ port } !` );
 } );

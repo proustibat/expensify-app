7fb56237d92b9a24dac3c5f4e2632d15a8d254d7
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-19 17:58:56 +0200

    Array data in firebase - part. 2

--- expensify-app/src/firebase/firebase.js
+++ expensify-app/src/firebase/firebase.js
@@ -1,20 +1,6 @@
 import * as firebase from 'firebase';
 import moment from "moment/moment";
 
-// Initialize Firebase
-const config = {
-    apiKey: "AIzaSyAozdfq3aCC2f66jG8Sb2QDWtMbV9bUN8Q",
-    authDomain: "prstbt-expensify-app.firebaseapp.com",
-    databaseURL: "https://prstbt-expensify-app.firebaseio.com",
-    projectId: "prstbt-expensify-app",
-    storageBucket: "prstbt-expensify-app.appspot.com",
-    messagingSenderId: "759437259231"
-};
-
-firebase.initializeApp( config );
-
-const database = firebase.database();
-
 const expenses = [ {
     description: 'Gum',
     note: 'Note for the gum',
@@ -32,6 +18,64 @@ const expenses = [ {
     createdAt: moment( 0 ).add( 4, 'days' ).valueOf()
 } ];
 
-expenses.forEach( expense => {
-    database.ref( 'expenses' ).push( expense );
+
+// Initialize Firebase
+const config = {
+    apiKey: "AIzaSyAozdfq3aCC2f66jG8Sb2QDWtMbV9bUN8Q",
+    authDomain: "prstbt-expensify-app.firebaseapp.com",
+    databaseURL: "https://prstbt-expensify-app.firebaseio.com",
+    projectId: "prstbt-expensify-app",
+    storageBucket: "prstbt-expensify-app.appspot.com",
+    messagingSenderId: "759437259231"
+};
+
+firebase.initializeApp( config );
+
+const database = firebase.database();
+
+// child_removed event
+database.ref( 'expenses' ).on( 'child_removed', snapshot => {
+    console.log( snapshot.key, snapshot.val() );
 } );
+
+// child_changed event
+database.ref( 'expenses' ).on( 'child_changed', snapshot => {
+    console.log( snapshot.key, snapshot.val() );
+} );
+
+// child_added event
+database.ref( 'expenses' ).on( 'child_added', snapshot => {
+    console.log( snapshot.key, snapshot.val() );
+} );
+
+//database.ref( 'expenses' )
+//    .once( 'value' )
+//    .then( snapshot => {
+//        //console.log( snapshot.val() );
+//        const expenses = [];
+//        snapshot.forEach( child => {
+//            expenses.push( {
+//                id: child.key,
+//                ...child.val()
+//            } );
+//        } );
+//        console.log( expenses );
+//    } );
+
+//database.ref( 'expenses' )
+//    .on( 'value', snapshot => {
+//        const expenses = [];
+//        snapshot.forEach( child => {
+//            expenses.push( {
+//                id: child.key,
+//                ...child.val()
+//            } );
+//        } );
+//        console.log( expenses );
+//    } );
+
+//expenses.forEach( expense => {
+//    database.ref( 'expenses' ).push( expense );
+//} );
+
+database.ref( 'expenses' ).push( expenses[0] );

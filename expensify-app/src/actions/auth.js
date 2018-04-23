0ab9e5ecb641346ba49aa9139e7a1d0c71b309c2
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-23 18:53:17 +0200

    Refacto Login Page and google authentication

--- expensify-app/src/actions/auth.js
+++ expensify-app/src/actions/auth.js
@@ -1,7 +1,3 @@
 import { firebase, googleProvider } from "../firebase/firebase";
 
-export const startLogin = () => {
-  return () => {
-    return firebase.auth().signInWithPopup( googleProvider );
-  };
-};
+export const startLogin = () => () => firebase.auth().signInWithPopup( googleProvider );

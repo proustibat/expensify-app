d3070f180cdc1e2d3b7775aeb4b1d7bc2e2d1019
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-24 17:27:40 +0200

    Summary area

--- expensify-app/src/components/ExpensesSummary.js
+++ expensify-app/src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from 'react-router-dom';
 import selectExpensesTotal from "../selectors/expenses-total";
 import getVisibleExpenses from '../selectors/expenses';
 import numeral from "numeral";
@@ -7,8 +8,13 @@ import numeral from "numeral";
 
 export const ExpensesSummary = ( { count, total } ) => {
     return (
-        <div>
-            <h1>Viewing { count } expense{ count > 1 ? 's' : ''} totalling { numeral( total / 100 ).format( '$0,0.00' ) }</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page-header__title">Viewing <span>{ count }</span> expense{ count > 1 ? 's' : ''} totalling <span>{ numeral( total / 100 ).format( '$0,0.00' ) }</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expense</Link>
+                </div>
+            </div>
         </div>
     );
 };
--- expensify-app/src/components/Header.js
+++ expensify-app/src/components/Header.js
@@ -11,7 +11,7 @@ export const Header = ( { startLogout } ) => (
                 <Link className="header__title" to="/dashboard" exact={ true } >
                     <h1>Expensify</h1>
                 </Link>
-                <button onClick={ startLogout }>Logout</button>
+                <button className="button button--link" onClick={ startLogout }>Logout</button>
             </div>
         </div>
     </header>

296954be7c41730c356b2d941fe9ff3481be28d9
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-24 16:45:29 +0200

    Login Page

--- expensify-app/src/components/LoginPage.js
+++ expensify-app/src/components/LoginPage.js
@@ -3,9 +3,12 @@ import { connect } from 'react-redux';
 import { startLogin } from "../actions/auth";
 
 export const LoginPage = props => (
-    <div>
-        <h1>LoginPage</h1>
-        <button onClick={ props.startLogin }>Login</button>
+    <div className="box-layout">
+        <div className="box-layout__box">
+            <h1 className="box-layout__title">Expensify App</h1>
+            <p>It's time to get your expenses under control!</p>
+            <button onClick={ props.startLogin }>Login</button>
+        </div>
     </div>
 );
 

f167dba4dd51f9124bb4c40d80d0383244c8e031
Jennifer Proust <jennifer.proust@gmail.com>
2018-04-24 12:00:44 +0200

    The auth reducer

--- expensify-app/src/actions/auth.js
+++ expensify-app/src/actions/auth.js
@@ -1,5 +1,16 @@
 import { firebase, googleProvider } from "../firebase/firebase";
 
+export const login = ( uid ) => {
+    return {
+        type: 'LOGIN',
+        uid
+    };
+};
+
+export const logout = () => ( {
+    type: 'LOGOUT'
+} );
+
 export const startLogin = () => () => firebase.auth().signInWithPopup( googleProvider );
 
 export const startLogout = () => () => firebase.auth().signOut();
--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import { Provider } from "react-redux";
-
 import AppRouter, { history } from './routers/AppRouter';
+
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
+import { firebase } from './firebase/firebase';
+import { login, logout } from "./actions/auth";
+
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-
-import { firebase } from './firebase/firebase';
-
 
 const store = configureStore();
 
@@ -34,7 +33,10 @@ ReactDOM.render( <p>Loading...</p>, document.querySelector( '#app' ) );
 
 firebase.auth().onAuthStateChanged( user => {
     if ( user ) {
-        console.log( 'log in' );
+        console.log( 'uid', user.uid );
+
+        store.dispatch( login( user.uid ) );
+
         store.dispatch( startSetExpenses() ).then( () => {
             renderApp();
             if ( history.location.pathname === '/' ) {
@@ -44,6 +46,7 @@ firebase.auth().onAuthStateChanged( user => {
     }
     else {
         console.log( 'log out' );
+        store.dispatch( logout() );
         renderApp();
         history.push( '/' );
     }
